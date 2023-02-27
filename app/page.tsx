8ecf316f04cfba969163f627d787f1805d39fe00
jiang-xia <963798512@qq.com>
2023-02-27 12:44:57 +0800

    feat:  添加tailwindcss

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from './page.module.css'
 export default function Home() {
   return (
     <main className={styles.main}>
       你好，江夏！
+      <Link href="/posts">posts</Link>
     </main>
   )
 }
